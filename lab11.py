fb160d1dfc6092fa8919617c14ef0e92487b4405
Bob <aa@aa>
2022-12-20 21:07:36 +0100

    Task 6 done

--- lab11.py
+++ lab11.py
@@ -130,3 +130,22 @@ wyswietl(C)
 
 # ==========================
 print("Zad 6")
+
+
+def getMatrixMinor(m,i,j):
+    return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
+
+
+def det(m):
+    if len(m) == 2:
+        return m[0][0]*m[1][1]-m[0][1]*m[1][0]
+
+    determinant = 0
+    for c in range(len(m)):
+        determinant += ((-1)**c)*m[0][c]*det(getMatrixMinor(m,0,c))
+    return determinant
+
+
+A = losowa(3, 3, 15)
+wyswietl(A)
+print("Wyznacznik:", det(A))
